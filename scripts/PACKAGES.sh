aa261328d804ce41b88ccd75fc2c452eb8a810e5
xidz <onexidz@gmail.com>
2025-05-22 17:58:32 +0700

    Update PACKAGES.sh

--- scripts/PACKAGES.sh
+++ scripts/PACKAGES.sh
@@ -66,6 +66,7 @@ packages_custom+=(
     "luci-theme-alpha_|https://api.github.com/repos/derisamedia/luci-theme-alpha/releases/latest"
     "luci-app-neko_|https://api.github.com/repos/nosignals/openwrt-neko/releases/latest"
     "luci-app-tailscale_|https://api.github.com/repos/asvow/luci-app-tailscale/releases/latest"
+    "luci-app-rakitanmanager_|https://api.github.com/repos/rtaserver/RakitanManager/releases/latest"
 )
 
 if [ "${TYPE}" == "OPHUB" ]; then
