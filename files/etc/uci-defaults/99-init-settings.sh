bf35cd007c502a9ccde98130706c104e6acfbe6d
xidz <onexidz@gmail.com>
2025-05-24 18:32:18 +0700

    Update 99-init-settings.sh

--- files/etc/uci-defaults/99-init-settings.sh
+++ files/etc/uci-defaults/99-init-settings.sh
@@ -139,10 +139,11 @@ echo "setup device amlogic"
 if opkg list-installed | grep -q luci-app-amlogic; then
   echo "luci-app-amlogic detected."
   rm -f /etc/profile.d/30-sysinfo.sh
-  sed -i '/exit 0/i #sleep 4 && /usr/bin/ledgpio -r' /etc/rc.local
+  sed -i '/exit 0/i #sleep 4 && /usr/bin/k5hgled -r' /etc/rc.local
+  sed -i '/exit 0/i #sleep 4 && /usr/bin/k6hgled -r' /etc/rc.local
 else
   echo "luci-app-amlogic no detected."
-  rm -f /usr/bin/ledgpio /usr/bin/gpioledon
+  rm -f /usr/bin/k5hgled /usr/bin/k6hgled /usr/bin/k5hgledon /usr/bin/k6hgledon
 fi
 
 # setup misc settings and permission
@@ -170,7 +171,7 @@ echo "restart netdata and vnstat"
 /etc/init.d/netdata restart && /etc/init.d/vnstat restart
 
 # TTL
-echo "run script ttl"
+echo "add and run script ttl"
 chmod +x /root/indowrt.sh && /root/indowrt.sh
 
 # setup tunnel installed
