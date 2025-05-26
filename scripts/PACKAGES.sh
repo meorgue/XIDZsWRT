2308b93285a8222dc2dddfd46d4af9850ed0d43d
xidz <onexidz@gmail.com>
2025-05-26 14:40:28 +0700

    Update PACKAGES.sh

--- scripts/PACKAGES.sh
+++ scripts/PACKAGES.sh
@@ -64,7 +64,6 @@ packages_custom+=(
     "luci-app-tinyfm_|https://api.github.com/repos/bobbyunknown/luci-app-tinyfm/releases/latest"
     "luci-app-droidnet_|https://api.github.com/repos/animegasan/luci-app-droidmodem/releases/latest"
     "luci-theme-alpha_|https://api.github.com/repos/derisamedia/luci-theme-alpha/releases/latest"
-    "luci-app-neko_|https://api.github.com/repos/nosignals/openwrt-neko/releases/latest"
     "luci-app-tailscale_|https://api.github.com/repos/asvow/luci-app-tailscale/releases/latest"
     "luci-app-rakitanmanager_|https://api.github.com/repos/rtaserver/RakitanManager/releases/latest"
 )
